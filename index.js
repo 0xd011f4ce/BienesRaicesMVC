a55e837f64a39d21e73c6adf6ce875510d7fa487
lain <lyx@lyxincyberia.org>
2024-08-07 15:52:17 -0500

    added a simple express server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hola, Mundo!");
+import express from "express";
+
+const app = express();
+
+// routing
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+// define port and start project
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://127.0.0.1:${port}`);
+});
