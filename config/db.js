6670285ae05d7324ca688301fc0a621f7f4765ed
lain <lyx@lyxincyberia.org>
2024-08-09 13:29:56 -0500

    moved database credentials to dotenv variables

--- config/db.js
+++ config/db.js
@@ -1,18 +1,26 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const db = new Sequelize("bienesraices_node_mvc", "root", "password", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  define: {
-    timestamps: true,
-  },
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+dotenv.config({ path: ".env" });
+
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: 3306,
+    dialect: "mysql",
+    define: {
+      timestamps: true,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
 export default db;
