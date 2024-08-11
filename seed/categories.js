5886060044adaf645ddff24780cc9b2d6561fc2b
lain <lyx@lyxincyberia.org>
2024-08-11 17:21:15 -0500

    added a seeder for categories, and prices

---
+++ seed/categories.js
@@ -0,0 +1,19 @@
+const categories = [
+  {
+    name: "House",
+  },
+  {
+    name: "Department",
+  },
+  {
+    name: "Office",
+  },
+  {
+    name: "Terrain",
+  },
+  {
+    name: "Cabin",
+  },
+];
+
+export default categories;
---
+++ seed/seeder.js
@@ -0,0 +1,33 @@
+import categories from "./categories.js";
+import prices from "./prices.js";
+
+import Category from "../models/category.js";
+import Price from "../models/price.js";
+
+import db from "../config/db.js";
+
+const importData = async () => {
+  try {
+    // authenticate
+    await db.authenticate();
+
+    // generate columns
+    await db.sync();
+
+    // insert data
+    await Promise.all([
+      Category.bulkCreate(categories),
+      Price.bulkCreate(prices),
+    ]);
+
+    console.log("Data imported successfully!");
+    process.exit();
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === "-i") {
+  importData();
+}
