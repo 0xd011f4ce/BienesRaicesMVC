e8aa48a27618ef47fa504bd0fb2e0f6b174fefa5
lain <lyx@lyxincyberia.org>
2024-08-11 13:28:42 -0500

    showing map

--- src/js/map.js
+++ src/js/map.js
@@ -1,1 +1,31 @@
-alert("I am the map");
+// (function () {
+//   const lat = 20;
+//   const lng = -103;
+//   const mapa = L.map("map").setView([lat, lng], 13);
+
+//   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//     maxZoom: 19,
+//     attribution:
+//       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+//   }).addTo(mapa);
+// })();
+
+(function () {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      async function (pos) {
+        const lat = pos.coords.latitude;
+        const lng = pos.coords.longitude;
+        const mapa = L.map("map").setView([lat, lng], 16);
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          maxZoom: 19,
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        }).addTo(mapa);
+      },
+      function (error) {
+        console.error(error);
+      }
+    );
+  }
+})();
