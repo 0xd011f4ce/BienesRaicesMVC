a82f73359418549baf6e0453567d4fe020c972ae
lain <lyx@lyxincyberia.org>
2024-08-12 10:05:08 -0500

    validating property creation on the route

--- controllers/propertiesController.js
+++ controllers/propertiesController.js
@@ -1,3 +1,5 @@
+import { validationResult } from "express-validator";
+
 import Price from "../models/price.js";
 import Category from "../models/category.js";
 
@@ -19,9 +21,32 @@ const propertyCreate = async (req, res) => {
   res.render("properties/create", {
     page: "Create Property",
     header: true,
+    csrfToken: req.csrfToken(),
     categories,
     prices,
   });
 };
 
-export { admin, propertyCreate };
+// save a new property
+const propertySave = async (req, res) => {
+  // validation result
+  let result = validationResult(req);
+
+  if (!result.isEmpty()) {
+    const [categories, prices] = await Promise.all([
+      Category.findAll(),
+      Price.findAll(),
+    ]);
+
+    return res.render("properties/create", {
+      page: "Create Property",
+      header: true,
+      csrfToken: req.csrfToken(),
+      categories,
+      prices,
+      errors: result.array(),
+    });
+  }
+};
+
+export { admin, propertyCreate, propertySave };
--- routes/propertiesRoutes.js
+++ routes/propertiesRoutes.js
@@ -1,13 +1,31 @@
 import express from "express";
+import { body } from "express-validator";
 
 import {
   admin,
   propertyCreate,
+  propertySave,
 } from "./../controllers/propertiesController.js";
 
 const router = express.Router();
 
 router.get("/my-properties", admin);
 router.get("/properties/create", propertyCreate);
+router.post(
+  "/properties/create",
+  body("title").notEmpty().withMessage("Ad Title is mandatory"),
+  body("description")
+    .notEmpty()
+    .withMessage("Description cannot be empty")
+    .isLength({ max: 255 })
+    .withMessage("Description cannot be longer than 255 characters"),
+  body("category").isNumeric().withMessage("Category is mandatory"),
+  body("price").isNumeric().withMessage("Price is mandatory"),
+  body("rooms").isNumeric().withMessage("Rooms is mandatory"),
+  body("garages").isNumeric().withMessage("Garages is mandatory"),
+  body("wc").isNumeric().withMessage("WC is mandatory"),
+  body("lat").notEmpty().withMessage("Locate property on the map"),
+  propertySave
+);
 
 export default router;
